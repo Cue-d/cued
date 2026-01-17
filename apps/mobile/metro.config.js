67faf51e0eac7412f51cd01ac46644db814ca665
Theo Tarr <theodore@tarr.com>
2026-01-17 13:38:55 -0500

    Fix mobile app startup errors by updating NativeWind config and removing MMKV
    - Update metro.config.js with proper NativeWind v5 options (inlineVariables, globalClassNamePolyfill)
    - Remove unused react-native-mmkv dependency that requires dev build

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,7 +1,13 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativewind } = require("nativewind/metro");
+const { withNativeWind } = require("nativewind/metro");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = withNativewind(config);
+module.exports = withNativeWind(config, {
+  input: "./src/global.css",
+  // inline variables break PlatformColor in CSS variables
+  inlineVariables: false,
+  // We add className support manually
+  globalClassNamePolyfill: false,
+});
